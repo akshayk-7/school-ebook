1d59e03399f81b0ec6d6bdb4c8849b284d833849
akshayk-7 <akshaykokitkar803@gmail.com>
2026-04-27 20:03:57 +0530

    added ch2 images

--- src/data/data.js
+++ src/data/data.js
@@ -1,12 +1,12 @@
 export const subjectMeta = {
-  maths: { name: "Mathematics", icon: "➗", subtitle: "Algebra, calculus, geometry" },
+  maths: { name: "Mathematics", icon: "➗", subtitle: "" },
   science: { name: "Science", icon: "🔬", subtitle: "Physics, chemistry, biology" },
   english: { name: "English", icon: "📚", subtitle: "Grammar, literature, writing" }
 };
 
 export const data = {
   maths: {
-    ch1: [
+    chapter_1: [
       "https://res.cloudinary.com/da5ykdcbk/image/upload/v1777222048/maths1_cczb6l.jpg",
       "https://res.cloudinary.com/da5ykdcbk/image/upload/q_auto/f_auto/v1777222048/maths2_d7cdze.jpg",
       "https://res.cloudinary.com/da5ykdcbk/image/upload/q_auto/f_auto/v1777222041/maths3_wjzt7l.jpg",
@@ -18,6 +18,23 @@ export const data = {
       "https://res.cloudinary.com/da5ykdcbk/image/upload/q_auto/f_auto/v1777222057/maths9_dcoxur.jpg",
       "https://res.cloudinary.com/da5ykdcbk/image/upload/q_auto/f_auto/v1777222041/maths10_myrwrh.jpg",
     ],
+    chapter_2: [
+      "https://res.cloudinary.com/da5ykdcbk/image/upload/q_auto/f_auto/v1777299938/maths11_umzopf.jpg",
+      "https://res.cloudinary.com/da5ykdcbk/image/upload/q_auto/f_auto/v1777299938/maths12_jeb5cc.jpg",
+      "https://res.cloudinary.com/da5ykdcbk/image/upload/q_auto/f_auto/v1777299941/maths13_hvjng6.jpg",
+      "https://res.cloudinary.com/da5ykdcbk/image/upload/q_auto/f_auto/v1777299939/maths14_eh4di4.jpg",
+      "https://res.cloudinary.com/da5ykdcbk/image/upload/q_auto/f_auto/v1777300023/maths15_pflukx.jpg",
+      "https://res.cloudinary.com/da5ykdcbk/image/upload/q_auto/f_auto/v1777299940/maths16_b5ltpn.jpg",
+      "https://res.cloudinary.com/da5ykdcbk/image/upload/q_auto/f_auto/v1777299942/maths17_fbzmlo.jpg",
+      "https://res.cloudinary.com/da5ykdcbk/image/upload/q_auto/f_auto/v1777299942/maths18_apqgmm.jpg",
+      "https://res.cloudinary.com/da5ykdcbk/image/upload/q_auto/f_auto/v1777299943/maths19_jrynvb.jpg",
+      "https://res.cloudinary.com/da5ykdcbk/image/upload/q_auto/f_auto/v1777299938/maths20_njr9gy.jpg",
+      "https://res.cloudinary.com/da5ykdcbk/image/upload/q_auto/f_auto/v1777299938/maths21_hpa8f0.jpg"
+
+
+
+
+    ]
 
   },
   science: {
